bb84c85aa106208158d51fc475a4f8bf8dfda227
MrJin <haosheng0211@gmail.com>
2023-03-05 01:36:52 +0800

    新增 electron 套件

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from "vue-router";
 
 const routes = [];
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: process.env.IS_ELECTRON
+    ? createWebHashHistory()
+    : createWebHistory(),
   routes,
 });
 
